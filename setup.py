2021ec9998aabeff8dc39526fb3114e4d1007c0c
Jamie Townsend <jamiehntownsend@gmail.com>
2019-02-06 21:31:16 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='fastar',
+    version='0.0',
+    author='Jamie Townsend',
+    author_email='jamiehntownsend@gmail.com',
+    packages=['fastar'],
+    install_requires=['numpy', 'jax'],
+    url='https://github.com/j-towns/fastar',
+    license='MIT',
+)
