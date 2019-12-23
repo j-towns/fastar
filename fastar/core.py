bd7179b9aa6f334529ab9d0ba39e60b4011608ae
Julius Kunze <juliuskunze@gmail.com>
2019-12-23 13:15:38 +0100

    Remove future imports

--- fastar/core.py
+++ fastar/core.py
@@ -1,7 +1,3 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 from contextlib import contextmanager
 from functools import reduce
 from operator import and_
--- tests/test_fastar.py
+++ tests/test_fastar.py
@@ -1,7 +1,3 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 import jax.lax as lax
 import jax.scipy.special as special
 import numpy as onp
